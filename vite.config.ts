5074154606782a8a5cebfa22659300359727b9d1
r3mot <cmendrella1@toromail.csudh.edu>
2022-11-29 10:17:21 -0800

    chore: add test dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, InlineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+interface ViteConfig extends UserConfig {
+  test: InlineConfig;
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTest.ts"],
+  },
+} as ViteConfig);
